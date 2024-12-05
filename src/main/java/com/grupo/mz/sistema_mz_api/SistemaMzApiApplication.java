ab7750e2c91c4277e900a2e1bf025b69a5fbfb95
ALEXHONORIO <alexhonorio.contato@gmail.com>
2024-12-04 21:52:08 -0300

    Criando o projeto

---
+++ src/main/java/com/grupo/mz/sistema_mz_api/SistemaMzApiApplication.java
@@ -0,0 +1,13 @@
+package com.grupo.mz.sistema_mz_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaMzApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaMzApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupo/mz/sistema_mz_api/SistemaMzApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo.mz.sistema_mz_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaMzApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
